4af66f16a11c973af6702e3c3e7ad98abd46d1ce
BachanPradhan <86775571+BachanPradhan@users.noreply.github.com>
2023-08-01 15:49:08 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,24 +1,28 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(`mongodb://127.0.0.1:27017/employee_review_db`);
+//mongoose.connect(`mongodb://127.0.0.1:27017/employee_review_db`);
 
-// const db = mongoose.connection;
+mongoose.connect(`mongodb+srv://bachanpradhan:bachanpradhan@bachan.5qe5p2k.mongodb.net/`);
 
-// db.on('error', console.error.bind(console, 'Error connecting to the db'));
+const db = mongoose.connection;
 
-// db.once('open', function () {
-//   console.log("Successfully connected to the Database");
-// });
+db.on('error', console.error.bind(console, 'Error connecting to the db'));
 
-// module.exports = db;
+db.once('open', function () {
+  console.log("Successfully connected to the Database");
+});
 
-const mongoose = require("mongoose");
-//mongoose.set('strictQuery', false);
-const mongoDB = "mongodb://127.0.0.1/contact_list_db"; 
+module.exports = db;
 
 
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
-  console.log('connected to DB');
-}
+
+// const mongoose = require("mongoose");
+// //mongoose.set('strictQuery', false);
+// const mongoDB = "mongodb://127.0.0.1/contact_list_db"; 
+
+
+// main().catch(err => console.log(err));
+// async function main() {
+//   await mongoose.connect(mongoDB);
+//   console.log('connected to DB');
+// }
